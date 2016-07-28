25ce6fd3df6b1a1aface3237ec1311c8f362e6a4
phongbm <phongbm.it@gmail.com>
2016-07-28 19:56:16 +0700

    config resource folder, refactor code

--- src/main/java/com/phongbm/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/phongbm/helloworld/controller/HelloWorldController.java
@@ -3,20 +3,12 @@ package com.phongbm.helloworld.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloWorldController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
         return "index";
-    }
-
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public ModelAndView hello() {
-        ModelAndView model = new ModelAndView("hello");
-        model.addObject("username", "Welcome!");
-        return model;
     }
 
 }
